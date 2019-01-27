56b85a9788e04909a36d858537115dacfff0a64d
Cindy Cortes <cindy.indins@gmail.com>
2019-01-26 17:55:34 -0700

    added an Error call back

--- src/index.js
+++ src/index.js
@@ -4,22 +4,30 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     constructor(props) {
         super(props); // need to use this every time we use a constructor
-        
+
         // this is the only time we do direct assignment 
         // to this.state. When we want to update, we use setState
-        this.state = { lat: null };
-    
+        this.state = { lat: null, errorMessage: '' };
+
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 // we called setstate!!
                 this.setState({ lat: position.coords.latitude });
-            }, 
-            (err) => console.log(err)
+            },
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
         );
     }
-    
-    render () { // method
-        return <div>Latitude: {this.state.lat} </div>;
+
+    render() { // method
+        return (
+            <div>
+                Latitude: {this.state.lat}
+                <br />
+                Error: {this.state.errorMessage}
+            </div>
+        );
     }
 }
 
