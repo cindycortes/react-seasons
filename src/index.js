536d90ef1b8c6bf793fa5881d02062084f6c605a
Cindy Cortes <cindy.indins@gmail.com>
2019-01-26 19:58:01 -0700

    added conditional statemtns for season in Season Display

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props); // need to use this every time we use a constructor
 
-        // this is the only time we do direct assignment 
-        // to this.state. When we want to update, we use setState
-        this.state = { lat: null, errorMessage: '' };
-
-    }
+    state = { lat: null, errorMessage: '' };
 
     componentDidMount() {
 
@@ -19,8 +14,6 @@ class App extends React.Component {
         );
     }
 
-
-
     render() { // method
 
         if (this.state.errorMessage && !this.state.lat) {
@@ -28,7 +21,10 @@ class App extends React.Component {
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat} </div>
+            return <SeasonDisplay lat={this.state.lat} /> 
+                                    // prop = value
+            // property from the state of the app component and passing it as a prop down into the season display
+            // can take a state from one component and pass it down as a prop to the child
         }
 
         return <div>Loading! </div>;
